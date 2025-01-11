ce886fc0be83e38188b7bed1b7b88df82bd0242d
Victor Jumbo <vpanchojs@gmail.com>
2025-01-11 16:15:35 -0500

    update

--- lib/bpstate.dart
+++ lib/bpstate.dart
@@ -2,4 +2,5 @@ library bpstate;
 
 export 'src/bloc_base.dart' show BlocBase;
 export 'src/bloc_provider.dart';
+export 'src/service_locator.dart';
 export 'src/utils/localization/app_localizations.dart';
